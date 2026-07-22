2c0bb0356754083438343963556043ad559d367a
anilton (RV2) <aniltonmagalhaes+rv2@gmail.com>
2026-07-22 10:06:27 -0300

    feat: define UUIDAutoField

--- src/apps/manager/apps.py
+++ src/apps/manager/apps.py
@@ -2,6 +2,6 @@ from django.apps import AppConfig
 
 
 class ManagementConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
+    default_auto_field = 'apps.manager.fields.UUIDAutoField'
     name = 'apps.manager'
     verbose_name = 'Gerenciamento'
