4d0d872fd9fc03af99fbb360f6182e86ea94ef50
anilton (RV2) <aniltonmagalhaes+rv2@gmail.com>
2026-08-05 12:10:02 -0300

    feat: cria listagem e edição de categorias

--- src/apps/core/views/categorias_view.py
+++ src/apps/core/views/categorias_view.py
@@ -1,18 +1,96 @@
-from django.db.models import Sum
-from django.views.generic import ListView
+from typing import Any
 
+from django.contrib import messages
+from django.db import models
+from django.db.models import Sum
+from django.db.models.functions import Cast
+from django.urls import reverse
+from django.views.generic import DetailView, ListView, UpdateView
+
+from apps.django_chartjs import chartjs
+
+
+from ..forms import categoria_forms
 from ..models import CategoriaDespesa
+
+__all__ = (
+    'CategoriasListView',
+    'CategoriaDetailView',
+    'CategoriasUpdateView',
+    'CategoriaBulkUpdateView',
+)
 
 
 class CategoriasListView(ListView):
-    model = CategoriaDespesa
     allow_empty = True
-    template_name = 'core/archive/categorias_list.html'
+    template_name = 'core/categorias/list.html'
 
     def get_queryset(self):
-        assert self.model is not None
         return (
             self.request.user.despesas
             .annotate(total=Sum('itens_despesas__value', default=0))
             .all()
         )
+
+
+def _create_line_chart(despesa: CategoriaDespesa, id: str = 'dashboard_line_id'):
+    line_data = [{
+        'label': despesa.title,
+        'data': list(despesa.itens_despesas.values(
+            x=Cast('date', models.CharField()),
+            y=Cast('value', models.FloatField()),
+        )),
+    }]
+    return chartjs.ChartjsLine(id, line_data)
+
+
+class CategoriaDetailView(DetailView):
+    template_name = 'core/categorias/detail.html'
+
+    def get_queryset(self):
+        return self.request.user.despesas.all()
+
+    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        categoria: CategoriaDespesa = self.get_object()
+        context['line_chart'] = _create_line_chart(categoria)
+        return context
+
+
+class CategoriasUpdateView(UpdateView):
+    template_name = 'core/categorias/form.html'
+    form_class = categoria_forms.UpdateCategoriaDespesaForm
+    slug_field = 'id'
+
+    def get_queryset(self):
+        return self.request.user.despesas
+
+    def get_success_url(self) -> str:
+        return reverse('categorias_list')
+
+
+class CategoriaBulkUpdateView(UpdateView):
+    template_name = 'core/categorias/formset.html'
+
+    form_class = categoria_forms.ItemDespesaBulkUpdateFormset  # type: ignore
+
+    def get_queryset(self):
+        return self.request.user.despesas.all()
+
+    def get_success_url(self) -> str:
+        categoria = self.get_object()
+        messages.success(self.request, f'Categoria {categoria.title}')
+        return reverse('categorias_list')
+
+    def form_invalid(self, form):
+        print(form.errors)
+        # a = categoria_forms.ItemDespesaBulkUpdateFormset()
+        # for title, err in a.errors:
+        #     messages.error(self.request, f'{title}: {err}')
+        response = super().form_invalid(form)
+        return response
+
+    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+
+        return context
