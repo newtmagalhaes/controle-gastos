5914ba6ea03686bb4b0e86bf02fef3e549bf2306
anilton (RV2) <aniltonmagalhaes+rv2@gmail.com>
2026-08-05 10:47:52 -0300

    feat: adiciona botões para seleção de período

--- src/apps/core/views/dashboard_view.py
+++ src/apps/core/views/dashboard_view.py
@@ -1,8 +1,8 @@
 from datetime import date
 
 from django.db import models
-from django.db.models.manager import Manager
 from django.db.models.functions import Cast, TruncMonth
+from django.db.models.manager import Manager
 from django.utils import timezone
 from django.views.generic import ListView
 
@@ -31,11 +31,37 @@ def _create_pie_chart(qs: Manager[CategoriaDespesa], id: str = 'dashboard_pie_id
     return chartjs.ChartjsPie(id, pie_data)
 
 
-class DashboardView(ListView):
+class PeriodFromParamsMixin:
+    period_field = 'periodo'
+
+    def get_context_data(self, **kwargs) -> dict:
+        context = super().get_context_data(**kwargs)  # type: ignore
+        context["period_field"] = self.get_period_field()
+        return context
+
+    def get_period_field(self) -> str:
+        return self.period_field
+
+    def get_date(self) -> date | None:
+        '''return date if period is in params'''
+        value = self.request.GET.get(self.get_period_field())  # type: ignore
+        try:
+            assert isinstance(value, str)
+            year, month = map(int, value.split('-', maxsplit=1))
+            return date(year, month, 1)
+
+        except Exception:
+            return
+
+
+class DashboardView(PeriodFromParamsMixin, ListView):
+    '''Exibe `CategoriaDespesa`s e `ItensDespesa`s do usuário
+    do `ano-mês` atual ou especificado
+    '''
     template_name = 'core/dashboard.html'
 
     def get_date(self) -> date:
-        return timezone.now().date()
+        return super().get_date() or timezone.now().date().replace(day=1)
 
     def get_available_dates(self) -> list[str]:
         return list(
@@ -45,11 +71,11 @@ class DashboardView(ListView):
         )
 
     def get_queryset(self):
-        d = self.get_date()
+        selected_date = self.get_date()
         return categoria_despesas.list_non_empty_categorias_from_date(
             self.request.user.despesas,
-            d.month,
-            d.year,
+            selected_date.month,
+            selected_date.year,
         )
 
     def get_context_data(self, **kwargs):
--- src/tests/core/dashboard_view_tests.py
+++ src/tests/core/dashboard_view_tests.py
@@ -1,5 +1,5 @@
+from datetime import date
 from http import HTTPStatus
-from typing import Iterable
 
 from django.db.models import QuerySet
 from django.template.response import TemplateResponse
@@ -24,26 +24,64 @@ def categoria(admin_user):
 
 
 def test_get_dashboard_today(dashboard, categoria, admin_client: Client):
-    now = timezone.now().date()
+    current_month = timezone.now().date()
     response = admin_client.get(dashboard)
 
     assert response.status_code == HTTPStatus.OK
     assert isinstance(response, TemplateResponse)
+
+    selected_date = response.context.get('selected_date')
+    assert isinstance(selected_date, date)
+    assert selected_date.month == current_month.month
+    assert selected_date.year == current_month.year
 
     assert (object_list := response.context.get('object_list'))
     assert isinstance(object_list, (QuerySet, list,))
 
     assert len(itens := object_list[0].itens_despesas.all()) == 1
-    assert itens[0].date.year == now.year
-    assert itens[0].date.month == now.month
+    assert itens[0].date.year == current_month.year
+    assert itens[0].date.month == current_month.month
+
+
+def _get_random_month(today: date) -> date:
+    from random import randint
+    value = today.month
+    while value == today.month:
+        value = randint(1, 12)
+    return today.replace(month=value)
+
+
+def test_get_dashboard_from_specific_month(dashboard, categoria, admin_client: Client):
+    random_month = _get_random_month(timezone.now().date().replace(day=1))
+    response = admin_client.get(dashboard, query_params={'periodo': random_month.strftime('%Y-%m')})  # type: ignore
+
+    assert response.status_code == HTTPStatus.OK
+    assert isinstance(response, TemplateResponse)
+
+    selected_date = response.context.get('selected_date')
+    assert isinstance(selected_date, date)
+    assert selected_date.month == random_month.month
+    assert selected_date.year == random_month.year
+
+    assert (object_list := response.context.get('object_list'))
+    assert isinstance(object_list, (QuerySet, list,))
+
+    assert len(itens := object_list[0].itens_despesas.all()) == 1
+    assert itens[0].date.year == random_month.year
+    assert itens[0].date.month == random_month.month
 
 
 def test_get_empty_dashboard_today(dashboard, admin_client: Client):
-    now = timezone.now().date()
+    current_month = timezone.now().date()
     response = admin_client.get(dashboard)
 
     assert response.status_code == HTTPStatus.OK
     assert isinstance(response, TemplateResponse)
+
+    selected_date = response.context.get('selected_date')
+    assert isinstance(selected_date, date)
+    assert selected_date.month == current_month.month
+    assert selected_date.year == current_month.year
 
     object_list = response.context.get('object_list')
     assert isinstance(object_list, (QuerySet, list,))
